029b8e6ccc31b03ac7730a55ed9eb4cdfea0f174
Charles <home@dhcp-vl2041-31930.eduroam.cornell.edu>
2022-07-28 21:59:45 -0400

    added author field in Book class and updated README

--- Book.java
+++ Book.java
@@ -1,20 +1,22 @@
 /** A class modeling a book. */
 public class Book {
     String title;
-
+    String author;
+    
     /** Create a new book with title b. */
-    public Book(String t) {
+    public Book(String t, String a) {
         title= t;
+        author= a;
     }
 
     /** Print this book. */
     public void printBook() {
-        System.out.println(title);
+        System.out.println(title + " " + author);
     }
 
     /** Some book examples. */
     public static void main(String[] args) {
-        Book b = new Book("A Clockwork Orange");
+        Book b = new Book("A Clockwork Orange", "Alexa Vanhattum");
         b.printBook();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 An example git repository for CS 2110.
+This project is about the Book.java class.
